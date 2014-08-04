365a9bc1e18b6ac9f2a4e5b50d1a57daaca82404
Dave Clayton <davedx@gmail.com>
2014-08-04 21:19:59 +0200

    Moved jQuery to npm dependency/package.json and required in in http module. Cleanup.

--- http.sim.js
+++ http.sim.js
@@ -1,11 +1,14 @@
+/**
+ * To be Simplicity compatible, all async methods SHOULD
+ * return a promise.
+ */
 define(function(require) {
 	var q = require("node_modules/q/q");
+	require("node_modules/jquery/dist/jquery");
 
   var http = {
     get: function(url, data) {
-      return q($.get(url, data, function(data, status) {
-        //console.log("Success: ", data);
-      }));
+      return q($.get(url, data));
     }
   };
 
